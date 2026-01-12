9ab934bc646d5bb90e1285dd3fcf24f6e23bb88f
Aniket Chakraborty <aniket.ac001@gmail.com>
2026-01-13 01:50:39 +0530

    Full Currency conversion app except the non woring api used

---
+++ Currency-converter/src/hooks/useCustomHook.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+export default function useCustomHook(currency) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res));
+  }, [currency]);
+}
